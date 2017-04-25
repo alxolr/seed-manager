20ca71b37ddfccfd162c9a6bf6d101f28bbe01de
Alexandru Olaru <aolaru@pentalog.fr>
2017-04-25 14:12:35 +0300

    Added the load-manager

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const SeedLoader = require('./modules/seed-loader');
 const LoadManager = require('./modules/load-manager');
 
-SeedLoader(['one', 'two', 'tree'], Number);
-
 module.exports = {
   SeedLoader,
   LoadManager,
--- modules/load-manager.js
+++ modules/load-manager.js
@@ -1,15 +1,22 @@
 const async = require('async');
 
-class LoaderManager {
-  constructor(loaders) {
-    this.loaders = loaders || [];
+function LoaderManager(loaders) {
+  this.loaders = loaders;
+
+  function add(loader) {
+    if (this.loaders.indexOf(loader) !== -1) {
+      this.loaders.push(loader);
+    }
   }
 
-  use(loader) {
-    this.loaders.push(loader);
+  function remove(loader) {
+    const position = this.loaders.indexOf(loader);
+    if (position !== -1) {
+      this.loaders.splice(position, 1);
+    }
   }
 
-  load() {
+  function load() {
     return new Promise((resolve, reject) => {
       async.each(this.loaders, (loader, cb) => {
         loader.drop()
@@ -23,7 +30,7 @@ class LoaderManager {
     });
   }
 
-  drop() {
+  function drop() {
     return new Promise((resolve, reject) => {
       async.each(this.loaders, (loader, cb) => {
         loader.drop()
@@ -34,6 +41,13 @@ class LoaderManager {
       });
     });
   }
+
+  return {
+    add,
+    remove,
+    load,
+    drop,
+  };
 }
 
 module.exports = LoaderManager;
